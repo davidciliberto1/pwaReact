3af2bfa3b95109743d53b936c438ea349cffc8ed
davidciliberto1 <davidciliberto@gmail.com>
2020-06-26 16:35:24 -0300

    practica state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState, Component } from "react";
+import Hols from "./componentes/saludos";
+import Contador from "./componentes/contador";
 
+const App = () => {
+  const saludo = "holaaaa lali te amoooooo";
+  const [monto, setMonto] = useState(2);
+  const [cuenta, setCuenta] = useState(4);
 
- const App = () => {
-        const saludo = 'holaaaa lali te amoooooo'
-    return (
-        
-           `${saludo} Holiiis ${saludo}${saludo}${saludo}${saludo}${saludo}${saludo}${saludo}${saludo}${saludo}`
-        
-    );
-}
+  function decreaseCuenta() {
+    setCuenta(cuenta - 1);
+  }
+
+  function aumentaCuenta() {
+    setCuenta(cuenta + 1);
+  }
+  function cambiaState() {
+         setMonto()
+  }
+  return (
+    <div>
+      <button onClick={decreaseCuenta}>-</button>
+      {cuenta}
+      <button onClick={aumentaCuenta}>+</button>
+      <br />
+      <input type='text' placeholder="ingrese nuevo monto..." onChange={cambiaState}></input>
+      <button>Añade monto</button>
+
+       <ul>
+              <li>
+                     {monto}
+              </li>
+       </ul>
+
+      <Hols />
+      {saludo}
+      <br />
+      Holiiis
+    </div>
+  );
+};
 
 export default App;
---
+++ src/componentes/saludos.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+
+function Hols() {
+    return <p>hola deiiiiiiivid</p>;
+}
+export default Hols;
